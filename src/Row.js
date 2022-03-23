2a3b0362dc5185256f6734e21a0de64a6bd3b0a5
juleni <69912179+juleni@users.noreply.github.com>
2022-03-23 12:54:14 +0100

    ADD: Trailer

--- src/Row.js
+++ src/Row.js
@@ -1,4 +1,6 @@
+import movieTrailer from "movie-trailer";
 import React, { useEffect, useState } from "react";
+import YouTube from "react-youtube";
 import axios from "./axios";
 import "./Row.css";
 
@@ -6,6 +8,7 @@ const BASE_URL = "https://image.tmdb.org/t/p/original/";
 
 function Row({ title, fetchUrl, isLargeRow = false }) {
   const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
 
   // A snippet of code which runs based on a specific condition/variable
   useEffect(() => {
@@ -20,21 +23,62 @@ function Row({ title, fetchUrl, isLargeRow = false }) {
     fetchData();
   }, [fetchUrl]);
 
-  console.log("movies = ", movies);
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    },
+  };
+
+  const handleOnClick = (movie) => {
+    if (trailerUrl) {
+      // close trailer if it is opened
+      setTrailerUrl("");
+    } else {
+      let foundMovieURL = false;
+      // try to find movie trailer for the movie name
+      movieTrailer(null, { tmdbId: movie?.id })
+        .then((url) => {
+          if (url != null) {
+            foundMovieURL = true;
+            // url example: https://www.youtube.com/watch?v=w7ejDZ8SWv8
+            const urlParams = new URLSearchParams(new URL(url).search);
+            // we need only last part of url from "v" parameter: "w7ejDZ8SWv8"
+            setTrailerUrl(urlParams.get("v"));
+          }
+        })
+        .catch((error) => console.log(error));
+      if (!foundMovieURL) {
+        // try to find trailer by name
+        movieTrailer(movie?.name || "")
+          .then((url) => {
+            if (url != null) {
+              foundMovieURL = true;
+              const urlParams = new URLSearchParams(new URL(url).search);
+              setTrailerUrl(urlParams.get("v"));
+            }
+          })
+          .catch((error) => console.log(error));
+      }
+    }
+  };
+
   return (
     <div className="row">
       <h2>{title}</h2>
       <div className="row__posters">
-        {/** row posters */}
         {movies.map(
           (movie) =>
             movie.poster_path &&
             movie.backdrop_path && (
               <img
+                key={movie.id}
+                onClick={() => handleOnClick(movie)}
                 className={`row__poster${
                   isLargeRow ? " row__posterLarge" : ""
                 }`}
-                key={movie.id}
                 src={`${BASE_URL}${
                   isLargeRow ? movie.poster_path : movie.backdrop_path
                 }`}
@@ -43,6 +87,7 @@ function Row({ title, fetchUrl, isLargeRow = false }) {
             )
         )}
       </div>
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
     </div>
   );
 }
